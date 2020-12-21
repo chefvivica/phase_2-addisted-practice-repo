40d866c231a8eb66f14fc650fecdfe25c4e9398e
chefvivica <vivicacorn@gmail.com>
2020-12-20 20:02:30 -0500

    added gitignore

---
+++ phase_2_assisted_practice_1.9/src/httpsessiondemo/LoginSession.java
@@ -0,0 +1,28 @@
+package httpsessiondemo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/LoginSession")
+public class LoginSession extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String userId = request.getParameter("userid");
+		HttpSession session = request.getSession();
+		session.setAttribute("userid", userId);
+		response.sendRedirect("DashboardSession");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
